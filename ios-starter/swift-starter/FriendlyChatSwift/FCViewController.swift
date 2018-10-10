162628032c3cd94e8c8e4d169f84cc49b6cbea27
kurikei <kurikei@gmail.com>
2018-10-10 17:38:52 +0900

    画像を送信する機能を追加した
    See Also:
    https://codelabs.developers.google.com/codelabs/firebase-ios-swift/index.html#7

--- ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
@@ -184,24 +184,44 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
     present(picker, animated: true, completion:nil)
   }
 
-  func imagePickerController(_ picker: UIImagePickerController,
-    didFinishPickingMediaWithInfo info: [String : Any]) {
-      picker.dismiss(animated: true, completion:nil)
+  @objc func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [String : Any]) {
+    picker.dismiss(animated: true, completion:nil)
     guard let uid = Auth.auth().currentUser?.uid else { return }
-
+    
     // if it's a photo from the library, not an image from the camera
     if #available(iOS 8.0, *), let referenceURL = info[UIImagePickerControllerReferenceURL] as? URL {
-      let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL], options: nil)
-      let asset = assets.firstObject
-      asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
-        let imageFile = contentEditingInput?.fullSizeImageURL
-        let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
-      })
+        let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL], options: nil)
+        let asset = assets.firstObject
+        asset?.requestContentEditingInput(with: nil, completionHandler: { [weak self] (contentEditingInput, info) in
+            let imageFile = contentEditingInput?.fullSizeImageURL
+            let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
+            guard let strongSelf = self else { return }
+            strongSelf.storageRef.child(filePath)
+                .putFile(from: imageFile!, metadata: nil) { (metadata, error) in
+                    if let error = error {
+                        let nsError = error as NSError
+                        print("Error uploading: \(nsError.localizedDescription)")
+                        return
+                    }
+                    strongSelf.sendMessage(withData: [Constants.MessageFields.imageURL: strongSelf.storageRef.child((metadata?.path)!).description])
+            }
+        })
     } else {
-      guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
-      let imageData = UIImageJPEGRepresentation(image, 0.8)
-      guard let uid = Auth.auth().currentUser?.uid else { return }
-      let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
+        let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        self.storageRef.child(imagePath)
+            .putData(imageData!, metadata: metadata) { [weak self] (metadata, error) in
+                if let error = error {
+                    print("Error uploading: \(error)")
+                    return
+                }
+                guard let strongSelf = self else { return }
+                strongSelf.sendMessage(withData: [Constants.MessageFields.imageURL: strongSelf.storageRef.child((metadata?.path)!).description])
+        }
     }
   }
 
